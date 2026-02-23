5e2dc646c4bfb502ce606feb0bace0bb795e73f6
Khalifa Chekkouch <chekkouch.khalifa@gmail.com>
2026-02-23 00:06:19 +0000

    Add files via upload

--- code.js
+++ code.js
@@ -11,7 +11,11 @@ generateBtn.addEventListener("click", () => {
     qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
     qrImg.addEventListener("load", () => {
         paperCard.classList.add("active");
-        generateBtn.innerText = "Generate QR Code";
+        generateBtn.innerHTML = `<svg  width="20" height="20" viewBox="0 0 24 24" fill="none"
+            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M21 12a9 9 0 1 1-9-9c2.52 0 4.93 1 6.74 2.74L21 8"></path>
+            <path d="M21 3v5h-5"></path>
+          </svg> Generate QR Code`;
     });
 }, 1000);
 });
